17ebdb1195e100178aeb2e0f30585b151c6ad872
André Fillype <andrefillype10@gmail.com>
2021-03-07 22:27:44 +0000

    share using device functionality

--- src/app/shared/components/weather-card/weather-card.component.ts
+++ src/app/shared/components/weather-card/weather-card.component.ts
@@ -6,15 +6,29 @@ import { ConsolidatedWeather } from 'src/app/models/interfaces/consolidated-weat
 @Component({
   selector: 'app-weather-card',
   templateUrl: './weather-card.component.html',
-  styleUrls: ['./weather-card.component.scss']
+  styleUrls: ['./weather-card.component.scss'],
 })
 export class WeatherCardComponent implements OnInit {
+  @Input() public weather: ConsolidatedWeather;
 
- @Input() public weather: ConsolidatedWeather;
+  @Input() public isShare = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  share(): void {
+    if (navigator.share) {
+      navigator
+        .share({
+          title: 'CleverWeather',
+          text: 'Everything about weather in your city.',
+          url: 'https://localhost:4201',
+        })
+        .then(() => console.log('Successful share'))
+        .catch((error) => console.log('Error sharing', error));
+    } else {
+      console.log('It is not possible share content in this device.');
+    }
   }
-
 }
